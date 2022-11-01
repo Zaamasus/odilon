14946875d5cf19b04f7c462f2442296762bf6734
Zaamasus <samuelgomes22442@gmail.com>
2022-11-01 14:21:34 -0300

    134
    Signed-off-by: Zaamasus <samuelgomes22442@gmail.com>

---
+++ index.js
@@ -0,0 +1,9 @@
+// --------------------------------------MODAL--------------------------
+ativar.addEventListener('click', () => {
+          modal1.classList.toggle('animar1');
+});
+
+
+ativar2.addEventListener('click', () => {
+          modal2.classList.toggle('animar2');
+});
