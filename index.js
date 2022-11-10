d6edea79612c0a5f0b63fc3166591832347650a2
Zaamasus <samuelgomes22442@gmail.com>
2022-11-10 14:19:34 -0300

    21212
    Signed-off-by: Zaamasus <samuelgomes22442@gmail.com>

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const handleClickOutside = (event) => {
-          let overlay = document.getElementById("overlay");
-          let modal = document.getElementById("modal");
-          if (!modal.contains(event.target)) {
-              modal.style.display = 'none';
-              overlay.style.display = 'none';
-              document.removeEventListener('click', handleClickOutside, false);
-          }
-      }
-      
-      const openModal = () => {
-          let overlay = document.getElementById("overlay");
-          let modal = document.getElementById("modal");
-          overlay.style.display = 'flex'
-          modal.style.display = 'flex'
-          setTimeout(() => { document.addEventListener('click', handleClickOutside, false) }, 200);
-      }
+    let overlay = document.getElementById("overlay");
+    let modal = document.getElementById("modal");
+    if (!modal.contains(event.target)) {
+        modal.style.display = 'none';
+        overlay.style.display = 'none';
+        document.removeEventListener('click', handleClickOutside, false);
+    }
+}
+
+const openModal = () => {
+    let overlay = document.getElementById("overlay");
+    let modal = document.getElementById("modal");
+    overlay.style.display = 'flex'
+    modal.style.display = 'flex'
+    setTimeout(() => { document.addEventListener('click', handleClickOutside, false) }, 200);
+}
+
