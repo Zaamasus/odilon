3871414f2e0cddd20adee6b06f764a289d775277
Zaamasus <samuelgomes22442@gmail.com>
2022-11-10 09:57:20 -0300

    add modal
    Signed-off-by: Zaamasus <samuelgomes22442@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-// --------------------------------------MODAL--------------------------
-ativar1.addEventListener('click', () => {
-          modal1.classList.toggle('animar');
-          
-});
+const handleClickOutside = (event) => {
+          let overlay = document.getElementById("overlay");
+          let modal = document.getElementById("modal");
+          if (!modal.contains(event.target)) {
+              modal.style.display = 'none';
+              overlay.style.display = 'none';
+              document.removeEventListener('click', handleClickOutside, false);
+          }
+      }
+      
+      const openModal = () => {
+          let overlay = document.getElementById("overlay");
+          let modal = document.getElementById("modal");
+          overlay.style.display = 'flex'
+          modal.style.display = 'flex'
+          setTimeout(() => { document.addEventListener('click', handleClickOutside, false) }, 200);
+      }
