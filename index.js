4103267dd216d42c2fda41f905f7d1acb2d95795
Zaamasus <samuelgomes22442@gmail.com>
2022-11-11 17:57:35 -0300

    477878
    Signed-off-by: Zaamasus <samuelgomes22442@gmail.com>

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const handleClickOutside = (event) => {
-    let overlay = document.getElementById("overlay");
-    let modal = document.getElementById("modal");
-    if (!modal.contains(event.target)) {
-        modal.style.display = 'none';
-        overlay.style.display = 'none';
-        document.removeEventListener('click', handleClickOutside, false);
-    }
-}
 
-const openModal = () => {
-    let overlay = document.getElementById("overlay");
-    let modal = document.getElementById("modal");
-    overlay.style.display = 'flex'
-    modal.style.display = 'flex'
-    setTimeout(() => { document.addEventListener('click', handleClickOutside, false) }, 200);
-}
 
+
+$('.nav a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+  });
+  
