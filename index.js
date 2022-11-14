d29925a909b1684a2e27c74008c2885ef4504adb
Zaamasus <samuelgomes22442@gmail.com>
2022-11-14 17:22:41 -0300

    carrocel
    Signed-off-by: Zaamasus <samuelgomes22442@gmail.com>

--- index.js
+++ index.js
@@ -1,31 +1,70 @@
-const carouselImages = document.querySelector('.carousel__images');
-const images = document.querySelectorAll('.carousel__images img');
-const carouselButtons = document.querySelectorAll('.carousel__button');
-const numberOfImages = document.querySelectorAll('.carousel__images img').length;
-let imageIndex = 1;
-let translateX = 0;
+//VIDEO THUMBS FUNCTIONS//
 
-carouselButtons.forEach(button => {
-  button.addEventListener('click', (event) => {
-    if (event.target.id === 'previous') {
-      if (imageIndex !== 1) {
-        imageIndex--;
-        translateX += 250;
-      }
-    } else {
-      if (imageIndex !== numberOfImages) {
-        imageIndex++;
-        translateX -= 250;
-      }
-    }
+$('.video-thumb:nth-child(1)').clone().appendTo('.video-thumbs').addClass('clone first-clone');
 
-    carouselImages.style.transform = `translateX(${translateX}px)`;
-    images.forEach((image, index) => {
-      if (index === imageIndex - 1) {
-        image.classList.add('active');
-      } else {
-        image.classList.remove('active');
-      }
-    });
-  });
-});
+$('.video-thumbs').find('.video-thumb:nth-child(5)').removeClass('active thumb-1').attr('index', '5');
+
+$('.video-thumb:nth-child(4)').clone().prependTo('.video-thumbs');
+
+$('.video-thumbs').find('.video-thumb:nth-child(1)').removeClass('active').addClass('clone last-clone').attr('index', '0');
+
+
+function nextVideoThumb() {
+	var currentThumb = parseInt($('.video-thumbs').attr('current-thumb'));
+	var totalThumbsInit = $('.video-thumb').length;
+	var totalThumbs = parseInt(totalThumbsInit - 2);
+	var activeClasses = $('.video-thumbs').attr('class');
+	var splitClasses = activeClasses.split(' ');
+	var thisClass = splitClasses[1];
+	
+	$('.video-thumb').removeClass('active');
+
+	if (currentThumb < totalThumbs) {
+		var nextThumb = parseInt(currentThumb + 1);
+		var nextClass = 'position-' + nextThumb;
+		$('.video-thumbs').attr('current-thumb', nextThumb);
+		$('.video-thumbs').removeClass(thisClass);
+		$('.video-thumbs').addClass(nextClass);
+		$('.video-thumb[index='+nextThumb+']').addClass('active');
+
+	} else if (currentThumb == totalThumbs) {
+		var nextThumb = 1;
+		var nextClass = 'position-1';
+		$('.video-thumbs').attr('current-thumb', nextThumb);
+		$('.video-thumbs').removeClass(thisClass);
+		$('.video-thumbs').addClass(nextClass);
+		$('.video-thumb[index='+nextThumb+']').addClass('active');
+	}
+
+};
+
+function prevVideoThumb() {
+	var currentThumb = parseInt($('.video-thumbs').attr('current-thumb'));
+	var totalThumbsInit = $('.video-thumb').length;
+	var totalThumbs = parseInt(totalThumbsInit - 2);
+	var activeClasses = $('.video-thumbs').attr('class');
+	var splitClasses = activeClasses.split(' ');
+	var thisClass = splitClasses[1];
+
+	if (currentThumb > 1) {
+		var prevThumb = parseInt(currentThumb - 1);
+		var prevClass = 'position-' + prevThumb;
+		$('.video-thumbs').attr('current-thumb', prevThumb);
+		$('.video-thumbs').removeClass(thisClass);
+		$('.video-thumbs').addClass(prevClass);
+		$('.video-thumb[index='+prevThumb+']').addClass('active');
+
+	} else if (currentThumb == 1) {
+		var prevThumb = totalThumbs;
+		var prevClass = 'position-' + totalThumbs;
+		$('.video-thumbs').attr('current-thumb', prevThumb);
+		$('.video-thumbs').removeClass(thisClass);
+		$('.video-thumbs').addClass(prevClass);
+		$('.video-thumb[index='+prevThumb+']').addClass('active');
+	}
+};
+
+$('.video-thumbs-frame').on('swipeleft', nextVideoThumb);
+$('.video-thumbs-frame').on('swiperight', prevVideoThumb);
+
+//
