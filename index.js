c10442b946b4fc4fd1ef37f62a59864833af7dd8
Zaamasus <samuelgomes22442@gmail.com>
2022-11-14 15:18:26 -0300

    telefone e Whats app
    Signed-off-by: Zaamasus <samuelgomes22442@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+const carouselImages = document.querySelector('.carousel__images');
+const images = document.querySelectorAll('.carousel__images img');
+const carouselButtons = document.querySelectorAll('.carousel__button');
+const numberOfImages = document.querySelectorAll('.carousel__images img').length;
+let imageIndex = 1;
+let translateX = 0;
 
+carouselButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    if (event.target.id === 'previous') {
+      if (imageIndex !== 1) {
+        imageIndex--;
+        translateX += 250;
+      }
+    } else {
+      if (imageIndex !== numberOfImages) {
+        imageIndex++;
+        translateX -= 250;
+      }
+    }
 
-
-$('.nav a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    var id = $(this).attr('href'),
-    targetOffset = $(id).offset().top;
-      
-    $('html, body').animate({ 
-      scrollTop: targetOffset - 100
-    }, 500);
+    carouselImages.style.transform = `translateX(${translateX}px)`;
+    images.forEach((image, index) => {
+      if (index === imageIndex - 1) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
+    });
   });
-  
+});
