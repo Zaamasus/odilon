a1c4b1c0301299e5c40c00ffa71b2eaffa0b81b7
Zaamasus <101902171+Zaamasus@users.noreply.github.com>
2022-11-15 01:16:23 -0300

    44444444
    Signed-off-by: Zaamasus <101902171+Zaamasus@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-var slideIndex = 1;
-showSlides(slideIndex);
-=======
 //VIDEO THUMBS FUNCTIONS//
 
 $('.video-thumb:nth-child(1)').clone().appendTo('.video-thumbs').addClass('clone first-clone');
@@ -11,25 +7,8 @@ $('.video-thumbs').find('.video-thumb:nth-child(5)').removeClass('active thumb-1
 $('.video-thumb:nth-child(4)').clone().prependTo('.video-thumbs');
 
 $('.video-thumbs').find('.video-thumb:nth-child(1)').removeClass('active').addClass('clone last-clone').attr('index', '0');
->>>>>>> d29925a909b1684a2e27c74008c2885ef4504adb
 
-// Next/previous controls
-function passaSlide(n) {
-  showSlides(slideIndex += n);
-}
 
-<<<<<<< HEAD
-function showSlides(n) {
-  var i;
-  var slides = document.querySelector(".mySlideVideo");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
-=======
 function nextVideoThumb() {
 	var currentThumb = parseInt($('.video-thumbs').attr('current-thumb'));
 	var totalThumbsInit = $('.video-thumb').length;
@@ -89,4 +68,3 @@ $('.video-thumbs-frame').on('swipeleft', nextVideoThumb);
 $('.video-thumbs-frame').on('swiperight', prevVideoThumb);
 
 //
->>>>>>> d29925a909b1684a2e27c74008c2885ef4504adb
